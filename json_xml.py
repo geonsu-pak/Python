28974519edc8183a44c78f711c97314269787bb3
geonsu-pak <82432733+geonsu-pak@users.noreply.github.com>
2021-11-07 02:08:45 +0900

    Delete json_xml.py

--- json_xml.py
+++
@@ -1,7 +0,0 @@
-import json
-serialized = """{ "title" : "Data Science Book",
-                  "author" : "Joel Grus",
-                  "publicationYear" : 2019,
-                  "topics" : [ "data", "science", "data science"] }"""
-deserialized = json.loads(serialized)
-assert deserialized["publicationYear"] == 2019
